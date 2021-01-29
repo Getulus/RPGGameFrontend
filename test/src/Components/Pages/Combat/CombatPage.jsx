19d33501f4610948076ff87256d55f4afed1eb59
Getulus <tamasgaj@gmail.com>
2021-01-29 15:46:39 +0100

    combat animation added 1.0

--- test/src/Components/Pages/Combat/CombatPage.jsx
+++ test/src/Components/Pages/Combat/CombatPage.jsx
@@ -8,7 +8,6 @@ import CombatPlayerStats from "./CombatPlayerStats";
 import CombatMonsterStats from "./CombatMonsterStats";
 import CombatLog from "./CombatLog";
 
-
 const CombatPage = () => {
   const [
     currentPlayer,
@@ -26,25 +25,75 @@ const CombatPage = () => {
   const [hideCombatPanel, setHideCombatPanel] = useState("none");
   const [startAdventure, setStartAdventure] = useState("Start Adventure");
   const [hideButton, setHideButton] = useState("inline");
-
+  const [playerHealthBar, setPLayerHealthBar] = useState("100%");
+  const [monsterHealthBar, setMonsterHealthBar] = useState("100%");
+  //const [roundCurrentPlayerHealth, setRoundCurrentPlayerHealth] = useState(currentPlayer.maxHealth)
   const history = useHistory();
 
   useEffect(() => {
     setUpdate(update + 1);
   }, []);
 
+  useEffect(() => {
+    moveBarAnimation()
+    setMonsterHealthBar("100%")
+  }, [combatLog]);
+
+  const calculateMonsterHealthBar = (currentHealth) => {
+    if (currentHealth <= 0) {
+      setMonsterHealthBar("0%");
+    } else {
+      setMonsterHealthBar(
+        Math.round((currentHealth / currentMonster.maxHealth) * 100) + "%"
+      );
+    }
+  };
+
+  const calculatePlayerHealthBar = (currentHealth) => {
+    if (currentHealth <= 0) {
+      setPLayerHealthBar("0%");
+    } else {
+      setPLayerHealthBar(
+        Math.round((currentHealth / currentPlayer.maxHealth) * 100) + "%"
+      );
+    }
+  };
+
+  const moveBarAnimation = () => {
+    
+    var round = 0;
+    var id = setInterval(frame, 800);
+    function frame() {
+      if (round >= combatLog.length) {
+        setHideButton("inline")
+        clearInterval(id);
+      } else {
+        console.log(combatLog[round])
+        if (combatLog[round]["name"] == "Player") {
+            calculateMonsterHealthBar(combatLog[round]["enemyRemainingHealth"])
+        } else {
+            calculatePlayerHealthBar(combatLog[round]["enemyRemainingHealth"])
+        }
+        round++
+      }
+    }
+  };
+
   const startCombat = () => {
+    setHideButton("none")
+    //calculatePlayerHealthBar();
     setHideCombatPanel("grid");
     setStartAdventure("Continue");
+
     if (currentPlayer.currentHealth <= 0) {
-      setHideButton("none")
+      //calculatePlayerHealthBar();
+      setHideButton("none");
       regenerate();
     } else {
       axios.get(`http://localhost:8762/charondor/action/combat`).then(() => {
         setLogUpdate(logUpdate + 1);
         setUpdate(update + 1);
-      });
-    }
+    })}
   };
 
   const regenerate = () => {
@@ -69,17 +118,37 @@ const CombatPage = () => {
         <div id="player-image" className="combat-panel image">
           <p className="character-name">{currentPlayer.name}</p>
           <img src={currentPlayer.image}></img>
+          <div class="w3-black">
+            <div
+              class="w3-red w3-center"
+              style={{ height: "20px", width: playerHealthBar }}
+            >
+              {playerHealthBar}
+            </div>
+          </div>
         </div>
+
         <div id="monster-image" className="combat-panel image">
           <p className="character-name">{currentMonster.name}</p>
           <img src={currentMonster.image}></img>
+          <div class="w3-black">
+            <div
+              class="w3-red"
+              style={{ height: "20px", width: monsterHealthBar }}
+            >
+              {monsterHealthBar}
+            </div>
+          </div>
         </div>
+
         <div id="player-stats" className="combat-panel info">
           <CombatPlayerStats></CombatPlayerStats>
         </div>
+
         <div id="monster-stats" className="combat-panel info">
           <CombatMonsterStats></CombatMonsterStats>
         </div>
+
         <div id="battle-log" className="combat-panel">
           <CombatLog></CombatLog>
         </div>
--- test/src/Components/Pages/Combat/CombatPlayerStats.jsx
+++ test/src/Components/Pages/Combat/CombatPlayerStats.jsx
@@ -19,7 +19,7 @@ const CombatPlayerStats = () => {
     <div className="combat-player-stats">
     <p>Player Info: </p>
       <p className="stat">Level: {currentPlayer.level}</p>
-      <p className="stat">Current Health: {currentPlayer.currentHealth}</p>
+      <p className="stat">Current Health: {currentPlayer.maxHealth}</p>
       <p className="stat">Attack Value: {currentPlayer.attackValue}</p>
       <p className="stat">Defense Value: {currentPlayer.defenseValue}</p>
       <p className="stat">Critical Chance: {currentPlayer.criticalChance}%</p>
